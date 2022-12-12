0351794dd6a828b997bdcdb52f6c78732ee7d0ce
Lucas Barq <lucasbarq@free.fr>
2022-12-12 16:27:59 +0100

    🏗️ making architecture for rreact

--- assets/react/index.tsx
+++ assets/react/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import App from "../components/exemple"
+import App from "./components/exemple"
 import {createRoot} from "react-dom/client";
+import Header from "./components/exemple";
 
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
+
+    <Header title="Hello World" />
 
 );
