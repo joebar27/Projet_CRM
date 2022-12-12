6a565d24e22a4287e015188e8bf75fa672ccc7d4
Lucas Barq <lucasbarq@free.fr>
2022-12-12 12:36:45 +0100

    ✅ create exemple file react

--- assets/react/index.js
+++ assets/react/index.js
@@ -1,8 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import App from "../components/exemple"
+import {createRoot} from "react-dom/client";
 
-ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    
-    document.getElementById("root")
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+
 );
