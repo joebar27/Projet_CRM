67d8bf77595f4b064d8d21021d84077cc733f7bf
Anger romain <anger.romain@gmail.com>
2023-01-13 14:08:11 +0100

    add table "Articles" and "Society"

---
+++ src/Entity/Articles.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticlesRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticlesRepository::class)]
+class Articles
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $availability = null;
+
+    #[ORM\Column]
+    private ?int $price_HT = null;
+
+    #[ORM\Column]
+    private ?int $TVA = null;
+
+    #[ORM\Column]
+    private ?int $quantity = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $updated_date = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $created_date = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $deleted_date = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getAvailability(): ?string
+    {
+        return $this->availability;
+    }
+
+    public function setAvailability(string $availability): self
+    {
+        $this->availability = $availability;
+
+        return $this;
+    }
+
+    public function getPriceHT(): ?int
+    {
+        return $this->price_HT;
+    }
+
+    public function setPriceHT(int $price_HT): self
+    {
+        $this->price_HT = $price_HT;
+
+        return $this;
+    }
+
+    public function getTVA(): ?int
+    {
+        return $this->TVA;
+    }
+
+    public function setTVA(int $TVA): self
+    {
+        $this->TVA = $TVA;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): self
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getUpdatedDate(): ?\DateTimeInterface
+    {
+        return $this->updated_date;
+    }
+
+    public function setUpdatedDate(\DateTimeInterface $updated_date): self
+    {
+        $this->updated_date = $updated_date;
+
+        return $this;
+    }
+
+    public function getCreatedDate(): ?\DateTimeInterface
+    {
+        return $this->created_date;
+    }
+
+    public function setCreatedDate(\DateTimeInterface $created_date): self
+    {
+        $this->created_date = $created_date;
+
+        return $this;
+    }
+
+    public function getDeletedDate(): ?\DateTimeInterface
+    {
+        return $this->deleted_date;
+    }
+
+    public function setDeletedDate(\DateTimeInterface $deleted_date): self
+    {
+        $this->deleted_date = $deleted_date;
+
+        return $this;
+    }
+}
