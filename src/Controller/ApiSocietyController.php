a0916678c607109538502dfa53f0ad51dc5c77fe
Anger romain <anger.romain@gmail.com>
2023-01-30 10:47:59 +0100

    resolve register bug

--- src/Controller/ApiSocietyController.php
+++ src/Controller/ApiSocietyController.php
@@ -2,17 +2,32 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\SocietyRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class ApiSocietyController extends AbstractController
 {
-    #[Route('/api/society', name: 'app_api_society')]
-    public function index(): Response
+    #[Route('/api/societies', name: 'api_societies')]
+    public function getAllSocieties(SocietyRepository $societyRepository): Response
     {
-        return $this->render('api_society/index.html.twig', [
-            'controller_name' => 'ApiSocietyController',
-        ]);
+        // récupération de toutes les societés en BDD
+        $society = $societyRepository->findAll();
+
+        return $this->json([$society,'status' => 'success', 'message' => 'success'], Response::HTTP_OK, );
+    }
+
+    #[Route('/api/addsociety', name: 'api_add_society')]
+    public function addSociety(SocietyRepository $societyRepository, Request $request, ): Response
+    {
+        // Ajout d'une societé en BDD :
+        $formData = $request->getContent();
+        $data = json_decode($formData, true);
+        // récupération d'un article en BDD
+        $society = $societyRepository->findOneBy(['id' => $data['id']]);
+        // affichage des society:
+        return $this->json([$society,'status' => 'success', 'message' => 'success'], Response::HTTP_OK, );
     }
 }
