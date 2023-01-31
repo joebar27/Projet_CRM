3a1dc82e1ef44c26cafd56d7aa78b684ea6ae752
Maëlys <maelys.ami@outlook.fr>
2023-01-31 09:10:41 +0100

    Add dynamic list, and some style

---
+++ assets/react/components/mainbody/deposits/Desposits.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import styled from 'styled-components'
+import SortingBar from './SortingBar'
+import Deposit from './Deposit'
+import AllDepositsBtn from '../AllDepositsBtn'
+
+const Deposits = ({ title, data, count }) => {
+
+    return (
+        <Container>
+            <Title>{title}<DepositsCount>{count}</DepositsCount></Title>
+            <SortingBar />
+            {data.map(deposit => (
+                <Deposit data={deposit} key={deposit.property.address.street} />
+            ))}
+            <AllDepositsBtn title={title} />
+        </Container>
+    )
+}
+
+
+const Container = styled.div`
+`
+
+const Title = styled.h1`
+    font-weight: 500;
+    color:  black;
+    font-size: 1.3rem;
+    display: flex;
+    align-items: center;
+`
+
+const DepositsCount = styled.div`
+    margin-left: 1rem;
+    font-size: 1rem;
+    background-color: #0a0047;
+    color: white;
+    width: 28px;
+    height: 28px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 20px;
+`
+
+export default Deposits
