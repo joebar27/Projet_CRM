e68a4d8ee16cff07829a1bf4b14d44da9f219207
SahelJ <sahel.ethnyca@live.fr>
2023-02-01 12:06:23 +0100

    fix init stripe

--- assets/react/components/Stripe/StripeContainer.tsx
+++ assets/react/components/Stripe/StripeContainer.tsx
@@ -7,6 +7,10 @@ const PUBLIC_KEY = "pk_test_51MP4JIDouJcaqTebjqTxwUqTXYocyNFFHre5VEEZn8CUa5IJiTK
 const stripeTestPromise = loadStripe(PUBLIC_KEY);
 
 const Stripe = ()=>{
+
+    // const option = {
+    //     clientSecret: 'pi_1JKS2Y2VYugoKSBzNHPFBNj9_secret_niLMVIt33lBGf0z6Gt5WIGc3C',
+    // };
     return(
         <Elements stripe={stripeTestPromise} >
             <CheckoutForm />
